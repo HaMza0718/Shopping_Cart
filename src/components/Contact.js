e0e6896291258d50de2b571e118f86d7ff06d846
Hamza Yasir <hamzayasir677@gmail.com>
2022-08-19 16:25:19 +0500

    third commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -14,23 +14,23 @@ const Contact = () => {
         <div className="box-container d-flex justify-content-between align-items-center">
           <div className="box m-auto">
             <h3>Our Branches</h3>
-            <a href="#">Tariq Road</a>
-            <a href="#">Hyper Star</a>
-            <a href="#">Milinium Mall</a>
+            <a href="/#">Tariq Road</a>
+            <a href="/#">Hyper Star</a>
+            <a href="/#">Milinium Mall</a>
           </div>
 
           <div className="box m-auto">
             <h3>Contact Info</h3>
-            <a href="#">+923472086321</a>
-            <a href="#">eid.collection@gmail.com</a>
-            <a href="#">Address: Main Branch tariq Road Karachi, Pakistan.</a>
+            <a href="/#">+923472086321</a>
+            <a href="/#">eid.collection@gmail.com</a>
+            <a href="/#">Address: Main Branch tariq Road Karachi, Pakistan.</a>
           </div>
 
           <div className="box m-auto">
             <h3>Follow on</h3>
-            <a href="#" id="follow" className="fab fa-facebook"></a>
-            <a href="#" id="follow" className="fab fa-twitter"></a>
-            <a href="#" id="follow" className="fab fa-instagram"></a>
+            <a href="/#" id="follow" className="fab fa-facebook"></a>
+            <a href="/#" id="follow" className="fab fa-twitter"></a>
+            <a href="/#" id="follow" className="fab fa-instagram"></a>
           </div>
         </div>
       </div>
